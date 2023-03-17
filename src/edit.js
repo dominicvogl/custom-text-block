720d2e14b1d34e03bfe8ffab49d3e4fc7fd0663d
Dominic Vogl <dominic.vogl@cat-ia.de>
2023-03-17 22:07:05 +0100

    add toolbar with many controls

--- src/edit.js
+++ src/edit.js
@@ -1,19 +1,96 @@
 // use the blockprops for the attributes on the html element for the editor and the frontend (classes, title, etc. Attributes)
 import { __ } from "@wordpress/i18n";
-import { useBlockProps, RichText } from "@wordpress/block-editor";
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+} from "@wordpress/block-editor";
+import {
+	ToolbarGroup,
+	ToolbarButton,
+	ToolbarDropdownMenu,
+} from "@wordpress/components";
 
 const Edit = ({ attributes, setAttributes }) => {
-	const { text } = attributes;
+	const { title } = attributes;
+	const { description } = attributes;
 
 	return (
-		<RichText
-			{...useBlockProps()}
-			tagName="h4"
-			onChange={(value) => setAttributes({ text: value })}
-			value={text}
-			placeholder={__("mey Text", "textbox")}
-			allowdFormats={["core/bold", "core/italic"]}
-		/>
+		<>
+			<section>
+				<BlockControls group="block">
+					<p>Block Controls</p>
+				</BlockControls>
+				<BlockControls group="inline">
+					<p>Inline Controls</p>
+				</BlockControls>
+				<BlockControls
+					controls={[
+						{
+							title: "Button 1",
+							icon: "admin-generic",
+							isActive: true,
+							onClick: () => console.log("Button 1 clicked"),
+						},
+						{
+							title: "Button 2",
+							icon: "smiley",
+							onClick: () => console.log("Button 2 clicked"),
+						},
+					]}
+				>
+					{description && (
+						<ToolbarGroup>
+							<ToolbarButton
+								title="Align Left"
+								icon="editor-alignleft"
+								onClick={() => console.log("Align Left")}
+							/>
+							<ToolbarButton
+								title="Align center"
+								icon="editor-aligncenter"
+								onClick={() => console.log("Align center")}
+							/>
+							<ToolbarButton
+								title="Align right"
+								icon="editor-alignright"
+								onClick={() => console.log("Align right")}
+							/>
+							<ToolbarDropdownMenu
+								icon="arrow-down-alt2"
+								label={__("Dropdown for more alignments", "textbox")}
+								controls={[
+									{
+										title: __("Wide", "textbox"),
+										icon: "align-wide",
+									},
+									{
+										title: __("Full", "textbox"),
+										icon: "align-full-width",
+									},
+								]}
+							/>
+						</ToolbarGroup>
+					)}
+				</BlockControls>
+				<RichText
+					{...useBlockProps()}
+					tagName="h3"
+					onChange={(value) => setAttributes({ title: value })}
+					value={title}
+					placeholder={__("mey Text", "textbox")}
+					allowedFormats={[]}
+				/>
+				<RichText
+					{...useBlockProps()}
+					tagName="p"
+					onChange={(value) => setAttributes({ description: value })}
+					value={description}
+					placeholder={__("mey Text", "textbox")}
+					allowedFormats={["core/bold", "core/italic"]}
+				/>
+			</section>
+		</>
 	);
 };
 
--- src/save.js
+++ src/save.js
@@ -1,9 +1,19 @@
 // use the blockprops for the attributes on the html element for the editor and the frontend (classes, title, etc. Attributes)
-import { useBlockProps } from "@wordpress/block-editor";
+import { useBlockProps, RichText } from "@wordpress/block-editor";
 
-const Save = () => {
+const Save = ({ attributes }) => {
+	const { title } = attributes;
+	const { description } = attributes;
+
 	return (
-		<p {...useBlockProps.save()}>Save JSX with this, Custom Block Stuff!</p>
+		<>
+			<RichText.Content {...useBlockProps.save()} tagName="h4" value={title} />
+			<RichText.Content
+				{...useBlockProps.save()}
+				tagName="p"
+				value={description}
+			/>
+		</>
 	);
 };
 
