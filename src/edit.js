3cf0757a461dd9f9e2086f28fd0f69c87466d276
Dominic Vogl <dominic.vogl@cat-ia.de>
2023-03-17 16:09:47 +0100

    make the text field, rich text

--- src/edit.js
+++ src/edit.js
@@ -1,8 +1,16 @@
 // use the blockprops for the attributes on the html element for the editor and the frontend (classes, title, etc. Attributes)
-import { useBlockProps } from "@wordpress/block-editor";
+import { __ } from "@wordpress/i18n";
+import { useBlockProps, RichText } from "@wordpress/block-editor";
 
 const Edit = () => {
-	return <p {...useBlockProps()}>Edit this Stuff!</p>;
+	return (
+		<RichText
+			{...useBlockProps()}
+			tagName="h4"
+			placeholder={__("mey Text", "textbox")}
+			allowdFormats={["core/bold", "core/italic"]}
+		/>
+	);
 };
 
 export default Edit;
